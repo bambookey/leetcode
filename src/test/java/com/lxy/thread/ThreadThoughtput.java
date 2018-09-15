512598a86a9aa299e3e501a117c4cdad05a22027
bambookey <lxyiiangu@126.com>
2018-09-16 00:42:40 +0800

    c

--- src/test/java/com/lxy/thread/ThreadThoughtput.java
+++ src/test/java/com/lxy/thread/ThreadThoughtput.java
@@ -1,13 +1,15 @@
 package com.lxy.thread;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 线程池吞吐量测试
- *
+ * <p>
  * 1000任务，每个任务5ms，
- *
+ * <p>
  * 1  5876
  * 2  2923
  * 4  1454
@@ -17,22 +19,84 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 32 202
  * 48 189
  * 64 187
- *
+ * <p>
  * 测试的机器为MacBookPro，四核八线程处理器
- * 大致可以看作是线程核心数等于CPU核心数的时候能够最大利用CPU性能，或线程核心数
- *
+ * 大致可以看作是线程核心数等于CPU核心数的时候能够最大利用CPU性能，或线程核心数+1
+ * <p>
+ * 实际上，日志收集中的处理策略，本质上是通过将请求根据id分散到了多个（单线程+缓冲队列）组合
+ * 以实现增大了吞吐量的目的
+ * <p>
+ * 跑了一次两种功能线程池的对比：前面的是并发线程池
+ * 可以发现二者性能几乎无差别
+ * i=1
+ * 5693
+ * 5951
+ * i=2
+ * 2914
+ * 2977
+ * i=4
+ * 1486
+ * 1476
+ * i=8
+ * 729
+ * 716
+ * i=16
+ * 385
+ * 379
+ * i=32
+ * 194
+ * 188
+ * i=64
+ * 100
+ * 101
+ * <p>
  * <p>
  * Created by lixiangyu on 2018/9/15.
  */
 public class ThreadThoughtput {
 
     private static int threadcount = 1000;
-    private static volatile CountDownLatch latch = new CountDownLatch(threadcount);
+    private static volatile CountDownLatch latch;
 
     public static void main(String[] args) throws InterruptedException {
+        for (int i = 1; i <= 64; i *= 2) {
+            System.out.println("i=" + i);
+            testThreadPool(i);
+            testParallelThreadPool(i);
+        }
+    }
+
+    public static void testParallelThreadPool(int poolSize) throws InterruptedException {
+        Map<Integer, ExecutorService> map = new HashMap<>(poolSize);
+        for (int i = 0; i < poolSize; i++) {
+            map.put(i, Executors.newFixedThreadPool(1));
+        }
+        latch = new CountDownLatch(threadcount);
         AtomicInteger cnt = new AtomicInteger(0);
 
-        int corePoolSize = 10;
+        long start = System.currentTimeMillis();
+        while (true) {
+            int index = cnt.addAndGet(1);
+            if (index > threadcount) {
+                break;
+            }
+            WorkThread workThread = new WorkThread();
+            map.get(index % poolSize).submit(workThread);
+        }
+        latch.await();
+        long cost = System.currentTimeMillis() - start;
+        System.out.println(cost);
+
+
+    }
+
+
+    public static void testThreadPool(int poolSize) throws InterruptedException {
+
+        latch = new CountDownLatch(threadcount);
+        AtomicInteger cnt = new AtomicInteger(0);
+
+        int corePoolSize = poolSize;
         int maximumPoolSize = corePoolSize;
         long keepAliveTime = 10L;
         TimeUnit unit = TimeUnit.SECONDS;
@@ -43,9 +107,9 @@ public class ThreadThoughtput {
 
         long start = System.currentTimeMillis();
 
-        while(true) {
+        while (true) {
             int index = cnt.addAndGet(1);
-            if(index > threadcount) {
+            if (index > threadcount) {
                 break;
             }
             // System.out.println(index);
@@ -55,10 +119,9 @@ public class ThreadThoughtput {
         latch.await();
         long cost = System.currentTimeMillis() - start;
         System.out.println(cost);
-        System.out.println("finished.");
     }
 
-     static class WorkThread implements Runnable {
+    static class WorkThread implements Runnable {
         @Override
         public void run() {
             try {
