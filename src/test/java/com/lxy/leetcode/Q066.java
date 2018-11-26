593f01e72aa1dde4107a7946671ba3aa863d3139
bambookey <lxyiiangu@126.com>
2018-11-26 22:10:33 +0800

    Q066

--- src/test/java/com/lxy/leetcode/Q066.java
+++ src/test/java/com/lxy/leetcode/Q066.java
@@ -1,10 +1,33 @@
 package com.lxy.leetcode;
 
+import java.util.Arrays;
+
 /**
  * Created by lixiangyu on 2018/11/25.
  */
 public class Q066 {
+    /**
+     * 大神的代码
+     *
+     * @param digits
+     * @return
+     */
     public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] out = new int[len + 1];
+        Arrays.fill(out, 0);
+        out[0] = 1;
+        return out;
+    }
+
+    public int[] plusOne2(int[] digits) {
         int add = 0;
         digits[digits.length - 1] += 1;
         for (int i = digits.length - 1; i >= 0; i--) {
@@ -19,9 +42,6 @@ public class Q066 {
         }
         int[] arr = new int[digits.length + 1];
         arr[0] = 1;
-        for (int i = 0; i < digits.length; i++) {
-            arr[i + 1] = digits[i];
-        }
         return arr;
     }
 }
