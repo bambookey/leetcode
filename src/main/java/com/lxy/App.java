132951fd040c98ccc1267d55f08eaca834366259
bambookey <lxyiiangu@126.com>
2018-12-14 14:26:04 +0800

    test
    test

--- src/main/java/com/lxy/App.java
+++ src/main/java/com/lxy/App.java
@@ -1,5 +1,10 @@
 package com.lxy;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        File file = new File("sss");
+        try {
+            BufferedInputStream s = new BufferedInputStream(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
